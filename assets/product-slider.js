5c15d06551eb6d63c3796c7af6888f0a20f07bfa
usamaayaz-arch <usama.ayaz@sanomads.com>
2026-03-05 13:21:18 +0500

    featured product slider

---
+++ assets/product-slider.js
@@ -0,0 +1,29 @@
+class ProductSlider extends HTMLElement {
+  connectedCallback() {
+    this.container = this.querySelector('.products');
+    this.backBtn = this.querySelector('.back_button');
+    this.forwardBtn = this.querySelector('.forward_button');
+
+    this.scrollAmount = () => this.container.clientWidth;
+
+    if (this.backBtn) {
+      this.backBtn.addEventListener('click', () => {
+        this.container.scrollBy({
+          left: -this.scrollAmount(),
+          behavior: 'smooth'
+        });
+      });
+    }
+
+    if (this.forwardBtn) {
+      this.forwardBtn.addEventListener('click', () => {
+        this.container.scrollBy({
+          left: this.scrollAmount(),
+          behavior: 'smooth'
+        });
+      });
+    }
+  }
+}
+
+customElements.define('product-slider', ProductSlider);
